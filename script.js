e000061ab055489b2c82508fdc4b4d14b1a92002
FasihAhmedRaza <thisfasih@gmail.com>
2024-10-04 00:39:55 +0500

    d

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let interruptDetected = false;
 // Function to handle sending query and receiving response
 async function sendQueryToServer(queryText) {
     try {
-        const response = await fetch('http://localhost:3000/chat', {
+        const response = await fetch('https://animation-bot-production.up.railway.app/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
